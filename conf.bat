c1bab4089f7754806b313ac84e912773909163bd
LETS\letic <letsdelfino@gmail.com>
2023-07-23 15:58:03 -0300

    updating README and creating a py file to star install programs

--- conf.bat
+++ conf.bat
@@ -1,5 +1,1 @@
-pip install -U openai-whisper
-pip install git+https://github.com/openai/whisper.git 
-
-choco install ffmpeg
-pip install setuptools-rust
+pip install whisper-mic
--- projeto.py
+++ projeto.py
@@ -1,2 +1,8 @@
 import os
-os.system("start cmd /K .\conf.bat")
+# os.system("start cmd /K .\conf.bat")
+
+from whisper_mic.whisper_mic import WhisperMic
+
+mic = WhisperMic()
+result = mic.listen_loop()
+print(result)
